2fac73920b12bca957a2a8d2c70046839a74e287
vinci <ukponoakpan270@gmail.com>
2020-11-17 12:50:34 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const firstname = 'ukpono';
+const lastname = 'Akpan';
+
+let val;
+val = firstname + ' ' +  lastname;
+
+// console.log(val)
+
+val = ' see';
+val += 'more';
+val = 'hello, my name is' +' ' + firstname + ' last' + ' ' + lastname;
+
+val = 'that\'s is a good thing';
+val = firstname.toUpperCase();
+val =  firstname.concat(' ', lastname);
+val = typeof firstname;
+ 
+
+
+
+console.log(val);
